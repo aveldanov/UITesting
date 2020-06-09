5c37125d00080c48c2147928a3b6d727e615b256
Anton Veldanov <anton.veldanov@gmail.com>
2020-06-09 00:06:07 -0700

    iOS

---
+++ BoardToDeath/BoardToDeathUITests/BoardToDeathUITests.swift
@@ -0,0 +1,27 @@
+//
+//  BoardToDeathUITests.swift
+//  BoardToDeathUITests
+//
+//  Created by Veldanov, Anton on 6/9/20.
+//  Copyright © 2020 Caleb Stultz. All rights reserved.
+//
+
+import XCTest
+
+class BoardToDeathUITests: XCTestCase {
+  
+  var app: XCUIApplication!
+
+    override func setUpWithError() throws {
+        app = XCUIApplication()
+        continueAfterFailure = false
+
+   
+    }
+
+    override func tearDownWithError() throws {
+
+    }
+
+
+}
